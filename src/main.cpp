780625489b9b392428293d226683a44d66bfac6e
Jonah Brüchert <jbb.prv@gmx.de>
2019-07-06 18:16:16 +0200

    Make kapptemplate compatible

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("KDE");
     QCoreApplication::setOrganizationDomain("kde.org");
-    QCoreApplication::setApplicationName("HelloKirigami");
+    QCoreApplication::setApplicationName("%{APPNAMELC}");
 
     QQmlApplicationEngine engine;
 
